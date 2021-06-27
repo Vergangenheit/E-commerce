b0960712813dd75d478d544c3fa569aa8293e320
Loloheia <lorenzo.ostano@omninext.it>
2021-06-27 16:57:56 +0200

    finished part1

---
+++ store/tests/test_views.py
@@ -0,0 +1,61 @@
+from unittest import skip
+from django.http.response import HttpResponse
+from django.http.request import HttpRequest
+from django.test import TestCase, Client, RequestFactory
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.core.handlers.wsgi import WSGIRequest
+from store.models import Category, Product
+from store.views import all_products
+
+
+class TestViewResponses(TestCase):
+
+    def setUp(self) -> None:
+        self.c = Client()
+        self.factory = RequestFactory()
+        User.objects.create(username='admin')
+        Category.objects.create(name='django', slug='django')
+        Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
+                               slug='django-beginners', price='20.00', image='django')
+
+    def test_url_allowed_hosts(self):
+        """
+            Test allowed hosts
+        :return: None
+        """
+        response: HttpResponse = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_url(self):
+        """
+            Test product response status
+        """
+        response: HttpResponse = self.c.get(reverse('store:product_detail', args=['django-beginners']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_detail_url(self):
+        """
+            Test category response status
+        """
+        response: HttpResponse = self.c.get(reverse('store:category_list', args=['django']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_html(self):
+        request = HttpRequest()
+        response: HttpResponse = all_products(request)
+        html: str = response.content.decode('utf8')
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_function(self):
+        request: WSGIRequest = self.factory.get('/item/django-beginners')
+        response: HttpResponse = all_products(request)
+        html: str = response.content.decode('utf8')
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
+
+
+
---
+++ store/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+from django.db.models import QuerySet
+from django.http import HttpResponse, HttpRequest
+from typing import Dict
+
+
+def categories(request: HttpRequest) -> Dict:
+    return {'categories': Category.objects.all()}
+
+
+def product_detail(request: HttpRequest, slug: str) -> HttpResponse:
+    product: Product = get_object_or_404(Product, slug=slug, in_stock=True)
+
+    return render(request, 'store/products/detail.html', {'product': product})
+
+
+# Create your views here.
+def all_products(request: HttpRequest) -> HttpResponse:
+    # grab the data
+    products: QuerySet = Product.objects.all()
+    # make that data available on my template
+    return render(request, 'store/home.html', {'products': products})
+
+
+def category_list(request: HttpRequest, category_slug: str) -> HttpResponse:
+    category: Category = get_object_or_404(Category, slug=category_slug)
+    products: QuerySet = Product.objects.filter(category=category)
+
+    return render(request, 'store/products/category.html', {'category': category, 'products': products})
